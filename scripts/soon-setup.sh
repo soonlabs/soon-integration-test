7915d0192cbe89917a2b3e319dc89f02f79b903f
Charles Ferrell <charlie@manta.network>
2024-12-15 11:09:43 -0500

    add logic to grab contract address from it-deploy.json
    Signed-off-by: Charles Ferrell <charlie@manta.network>

--- scripts/soon-setup.sh
+++ scripts/soon-setup.sh
@@ -4,6 +4,10 @@ if [[ -z "${SOON_PATH}" ]]; then
     export SOON_PATH=../soon
 fi
 
+if [[ -z "${DEPLOYMENT_PATH}" ]]; then
+    export DEPLOYMENT_PATH=./deployments/it-deploy.json
+fi
+
 # set some env variables
 export SOON_NODE_STORAGE_PATH=./.soon
 export SOON_NODE_L1_RPC_URL=http://localhost:8545
@@ -22,11 +26,16 @@ export ADMIN_SECRET_KEY="0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f46
 export DA_SERVER_RPC=""
 
 # get env variables from deployment file
-export L2OO_ADDRESS="0xc6e7DF5E7b4f2A278906862b61205850344D4e7d"
-export L1_STANDARD_BRIDGE_PROXY="0x0B306BF915C4d645ff596e518fAf3F9669b97016"
-export SYSTEM_CONFIG_PROXY="0x9A676e781A523b5d0C0e43731313A708CB607508"
-export L1_CROSS_DOMAIN_PROXY="0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1"
-export OPTIMISM_PORTAL_PROXY="0x0DCd1Bf9A1b36cE34237eEaFef220932846BCD82"
+# map the .env variables to it-deploy.json file generated from foundry
+declare -A env_var=( ["L2OO_ADDRESS"]="L2OutputOracleProxy" ["L1_STANDARD_BRIDGE_PROXY"]="L1StandardBridgeProxy" ["SYSTEM_CONFIG_PROXY"]="SystemConfigProxy" ["L1_CROSS_DOMAIN_PROXY"]="L1CrossDomainMessengerProxy" )
+
+for key in "${!env_var[@]}";
+
+do
+    VAR="${env_var[$key]}";
+    ADDRESS=$(jq -r ".${VAR}" $DEPLOYMENT_PATH);
+    export $key=${ADDRESS}
+done
 
 export SOON_NODE_DEPOSIT_CONTRACT=${OPTIMISM_PORTAL_PROXY}
 export SOON_NODE_SYSTEM_CONFIG_CONTRACT=${SYSTEM_CONFIG_PROXY}
